9372bbdca38e17502083f4495974a910b9d44ff6
gyulambaryanartur <gyulambaryan.artur@gmail.com>
2019-08-28 02:30:03 +0400

    home

---
+++ week2/problem7.py
@@ -0,0 +1,8 @@
+str1="How are you John?"
+name='Arthur'
+#str2=str1[:str1[:-1].find(' ')
+"""print("# of b: %s"%user.count("b"))
+print("# of c: %s"%user.count("c"))
+print("# of d: %s"%user.count("d"))
+print("# of e: %s"%user.count("e"))"""
+print(str1[:len(str1)- str1[::-1].find(' ')]+" " +name +"?")
